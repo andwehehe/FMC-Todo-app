c8528f3de4255962efa777e08e562708094ef44e
andwe <andreigobres3@gmail.com>
2025-11-11 17:34:20 +0800

    added theme toggle and desktop layout

---
+++ script.js
@@ -0,0 +1,67 @@
+// Checked function
+const checkmarks = document.querySelectorAll(".checkmark");
+const tasks = document.querySelectorAll(".task");
+
+checkmarks.forEach((checkmark, index) => {
+  checkmark.addEventListener('change', () => {
+    if(checkmark.checked) {
+      tasks[index].style.textDecoration = "line-through";
+      tasks[index].style.color = "hsl(235, 19%, 35%)";
+    } else {
+      tasks[index].style.textDecoration = "";
+      tasks[index].style.color = "";
+    }
+  })
+})
+
+// Header Image Switching
+function changeSize() {
+  const headerImg = document.querySelector(".header-image");
+  const themeToggle = document.querySelector(".light-dark-checkbox");
+  
+  const images = {
+    dark: {
+      desktop: "assets/images/bg-desktop-dark.jpg",
+      mobile: "assets/images/bg-mobile-dark.jpg"
+    },
+    light: {
+      desktop: "assets/images/bg-desktop-light.jpg",
+      mobile: "assets/images/bg-mobile-light.jpg"
+    }
+  }
+
+  const mobileSize = window.matchMedia("(max-width: 768px)");
+
+  let selectedMobile;
+  let selectedDesktop;
+  
+  if(themeToggle.checked) {
+    selectedDesktop = images.light.desktop;
+    selectedMobile = images.light.mobile;
+  } else {
+    selectedDesktop = images.dark.desktop;
+    selectedMobile = images.dark.mobile;
+  }
+
+  if(mobileSize.matches) {
+    headerImg.src = selectedMobile;
+  } else {
+    headerImg.src = selectedDesktop;
+  }
+}
+
+changeSize();
+window.addEventListener('resize', changeSize);
+
+// Theme Toggle
+const themeToggle = document.querySelector(".light-dark-checkbox");
+const body = document.querySelector("body");
+themeToggle.addEventListener('change', () => {
+    if(themeToggle.checked) {
+      changeSize();
+      body.classList.add("light-theme");
+    } else {
+      changeSize();
+      body.classList.remove("light-theme");
+    }
+})
